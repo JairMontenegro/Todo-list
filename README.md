0bcbe449af95129118f8c9b12eb422c7617f7d56
Jair montenegro <jairmontenegrof@gmail.com>
2022-08-02 12:42:10 -0300

    remove .DS_Store files

---
+++ README.md
@@ -0,0 +1,49 @@
+# TO DO APP REFACTORED WITH REACT || TO-DO APP REFACTORIZADA CON REACT
+
+![CAPTURE](/Todo%20App.png "APP SCREENSHOT")
+
+---
+
+:us:
+
+---
+
+[TO-DO JS](https://jairmontenegro.github.io/Todo-list// "click to enter")
+
+This is my first project that i used to learn how to do a javascript to-do List . The goal is use methods such as thrim, json stringfy, json parse, query selector, what's a callback, splice.
+
+This project was built with vanilla javascript and then refactored with react. You can see that project in the link below.
+[TO-DO APP REACT](https://jairmontenegro.github.io/To-Do-App-React-r/ "click to enter")
+
+## TO RUN THE APP AND EDIT
+
+---
+
+- only you need to run git clone command in terminal
+
+---
+
+:round_pushpin: [My Linkedin](https://www.linkedin.com/in/jair-montenegro-2a9499218/ "Jair Montenegro Florez")
+
+---
+
+# ESPAÑOL :es:
+
+[TO-DO JS](https://jairmontenegro.github.io/Todo-list// "click to enter")
+
+Este es mi primer proyecto que utilicé para aprender a hacer una lista de tareas de JavaScript. El objetivo es utilizar métodos como trim, json stringify, json parse, query selector, callback, splice.
+
+Este proyecto fue creado con javascript vanilla y luego aqui lo refactorize con react.
+Puede ver este ese proyecto en el link de abajo.
+
+[TO-DO APP REACT](https://jairmontenegro.github.io/To-Do-App-React-r/ "click to enter")
+
+## PARA EJECUTAR LA APLICACION Y EDITAR
+
+---
+
+- Solo necesitas ejecutar el comando git clone en la terminal
+
+---
+
+:round_pushpin: [Mi Linkedin](https://www.linkedin.com/in/jair-montenegro-2a9499218/ "Jair Montenegro Florez")
--- script.js
+++ script.js
@@ -1,72 +1,66 @@
 const inputBox = document.querySelector(".inputField input");
 const addBtn = document.querySelector(".inputField button");
 const todoList = document.querySelector(".todoList");
-const deleteAllBtn=document.querySelector(".footer button")
+const deleteAllBtn = document.querySelector(".footer button");
 
-
-inputBox.onkeyup = ()=>{
-    let userData = inputBox.value; 
-    if(userData.trim() !=0){
-    addBtn.classList.add("active")
-    }
-    else{
-        addBtn.classList.remove("active")
-    }
-}
+inputBox.onkeyup = () => {
+  let userData = inputBox.value;
+  if (userData.trim() != 0) {
+    addBtn.classList.add("active");
+  } else {
+    addBtn.classList.remove("active");
+  }
+};
 showTasks();
-addBtn.onclick =()=>{
-    let userData = inputBox.value;
-    let getLocalStorage = localStorage.getItem("New Todo");
-    if(getLocalStorage == null){
-        listArr =[];
-    } else{
-        listArr = JSON.parse(getLocalStorage);
-    }
-    listArr.push(userData);
-    localStorage.setItem("New Todo", JSON.stringify(listArr));
-    showTasks();
-    addBtn.classList.remove("active")
-}
+addBtn.onclick = () => {
+  let userData = inputBox.value;
+  let getLocalStorage = localStorage.getItem("New Todo");
+  if (getLocalStorage == null) {
+    listArr = [];
+  } else {
+    listArr = JSON.parse(getLocalStorage);
+  }
+  listArr.push(userData);
+  localStorage.setItem("New Todo", JSON.stringify(listArr));
+  showTasks();
+  addBtn.classList.remove("active");
+};
 
-//ADD TASK 
-function showTasks(){
-    let getLocalStorage = localStorage.getItem("New Todo");
-     if(getLocalStorage == null){
-        listArr =[];
-    } else{
-        listArr = JSON.parse(getLocalStorage);
-    }
-    const pendingNumb = document.querySelector(".pendingNumb")
-    pendingNumb.textContent =listArr.length;
-    if(listArr.length > 0){
-        deleteAllBtn.classList.add("active")
-    }else{
-        deleteAllBtn.classList.remove("active")
-    }
+//ADD TASK
+function showTasks() {
+  let getLocalStorage = localStorage.getItem("New Todo");
+  if (getLocalStorage == null) {
+    listArr = [];
+  } else {
+    listArr = JSON.parse(getLocalStorage);
+  }
+  const pendingNumb = document.querySelector(".pendingNumb");
+  pendingNumb.textContent = listArr.length;
+  if (listArr.length > 0) {
+    deleteAllBtn.classList.add("active");
+  } else {
+    deleteAllBtn.classList.remove("active");
+  }
 
-
-    let newLitag ="";
-    listArr.forEach((element, index) => {
-        newLitag +=`<li> ${element}<span onclick = "deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
-    });
-    todoList.innerHTML = newLitag;
-    inputBox.value=""; 
- 
+  let newLitag = "";
+  listArr.forEach((element, index) => {
+    newLitag += `<li> ${element}<span onclick = "deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
+  });
+  todoList.innerHTML = newLitag;
+  inputBox.value = "";
 }
 //DELETE TASK
-function deleteTask(index){
-    let getLocalStorage = localStorage.getItem("New Todo");
-    listArr = JSON.parse(getLocalStorage);
-    listArr.splice(index, 1);
-    localStorage.setItem("New Todo", JSON.stringify(listArr));
-    showTasks();
-
+function deleteTask(index) {
+  let getLocalStorage = localStorage.getItem("New Todo");
+  listArr = JSON.parse(getLocalStorage);
+  listArr.splice(index, 1);
+  localStorage.setItem("New Todo", JSON.stringify(listArr));
+  showTasks();
 }
 
-//delete all task 
-deleteAllBtn.onclick = () =>{
-    listArr =[];
-    localStorage.setItem("New Todo", JSON.stringify(listArr));
-    showTasks();
-
-}
+//delete all task
+deleteAllBtn.onclick = () => {
+  listArr = [];
+  localStorage.setItem("New Todo", JSON.stringify(listArr));
+  showTasks();
+};
